960211566e63858305089aa7b945da4f967e2862
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-11-04 04:12:36 +0530

    Added decorators to classes

--- persons.rb
+++ persons.rb
@@ -1,13 +1,22 @@
-class Person
+require_relative './nameable'
+require_relative './trimmer_decorator'
+require_relative './capitalize_decorator'
+
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id
+
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  attr_accessor :name, :age
-  attr_reader :id
+  def correct_name
+    @name
+  end
 
   private
 
@@ -21,3 +30,10 @@ class Person
     of_age? || @parent_permission
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmedperson = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmedperson.correct_name
